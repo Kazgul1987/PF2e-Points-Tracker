cf20614d46599df76f2842b98dca6ef66b3ef61e
Kazgul1987 <71765137+Kazgul1987@users.noreply.github.com>
2025-10-20 15:55:37 +0200

    Add Foundry module skeleton

---
+++ module/main.js
@@ -0,0 +1,9 @@
+const MODULE_ID = "pf2e-points-tracker";
+
+Hooks.once("init", () => {
+  console.log(`${MODULE_ID} | Initializing PF2e Points Tracker module.`);
+});
+
+Hooks.once("ready", () => {
+  console.log(`${MODULE_ID} | PF2e Points Tracker is ready.`);
+});
